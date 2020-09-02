988a7a464cf838c1515e4e5617d8e442678b7d3f
bruno maia <bruno_engineer@hotmail.com>
2020-09-02 11:43:57 -0300

    add - new database example, guide and requirements

--- flask/app.py
+++ flask/app.py
@@ -6,35 +6,30 @@ import pprint as pp
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://postgres:passwd123@192.168.0.11:5432/testingdatabase"
+] = "postgresql://postgres:passwd123@192.168.0.11:5432/test"
 db = SQLAlchemy(app)
 
 
-class Drivers(db.Model):
+class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.name
 
 
-db.session.add(Drivers(first_name="Old Mr.", last_name="PotatoHead"))
-db.session.commit()
-drivers = Drivers.query.all()
-for driver in drivers:
-    pp.pprint(driver.last_name)
+db.create_all()
+
+# TODO: creating first sample user
+# db.session.add(Person(name="Old Mr. PotatoHead"))
+# db.session.commit()
+
+person = Person.query.first()
 
 
 @app.route("/")
 def index():
-    drivers = Drivers.query.all()
-    pp.pprint(drivers)
-    return f"drivers found: {len(drivers)}"
-
-
-# class Vehicle(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     driver_id = db.Column(db.Integer, primary_key=True)
-#     make = db.Column(db.String, nullable=False)
-#     model = db.Column(db.String, unique=True, nullable=False)
+    return f"Hello, {person.name}!"
 
 
 if __name__ == "__main__":
---
+++ flask/app_conn_test.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pprint as pp
+
+
+app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:passwd123@192.168.0.11:5432/testingdatabase"
+db = SQLAlchemy(app)
+
+
+class Drivers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, unique=True, nullable=False)
+
+
+db.session.add(Drivers(first_name="Old Mr.", last_name="PotatoHead"))
+db.session.commit()
+drivers = Drivers.query.all()
+for driver in drivers:
+    pp.pprint(driver.last_name)
+
+potatoes = Drivers.query.filter_by(last_name="PotatoHead").all()
+for potato in potatoes:
+    db.session.delete(potato)
+
+db.session.commit()
+
+drivers = Drivers.query.all()
+
+
+@app.route("/")
+def index():
+    drivers = Drivers.query.all()
+    pp.pprint(drivers)
+    return f"drivers found: {len(drivers)}"
+
+
+# class Vehicle(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     driver_id = db.Column(db.Integer, primary_key=True)
+#     make = db.Column(db.String, nullable=False)
+#     model = db.Column(db.String, unique=True, nullable=False)
+
+
+if __name__ == "__main__":
+    app.run()
