5a963ba1c5b97c7b8117a66993e6eea516dfdbad
bruno maia <bruno_engineer@hotmail.com>
2020-09-01 23:11:20 -0300

    update - flask app now consumes postgresql

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,40 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import pprint as pp
+
 
 app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://postgres:passwd123@192.168.0.11:5432/testingdatabase"
+db = SQLAlchemy(app)
+
+
+class Drivers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, unique=True, nullable=False)
+
+
+db.session.add(Drivers(first_name="Old Mr.", last_name="PotatoHead"))
+db.session.commit()
+drivers = Drivers.query.all()
+for driver in drivers:
+    pp.pprint(driver.last_name)
 
 
 @app.route("/")
 def index():
-    return f"Hello, World!"
+    drivers = Drivers.query.all()
+    pp.pprint(drivers)
+    return f"drivers found: {len(drivers)}"
+
+
+# class Vehicle(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     driver_id = db.Column(db.Integer, primary_key=True)
+#     make = db.Column(db.String, nullable=False)
+#     model = db.Column(db.String, unique=True, nullable=False)
 
 
 if __name__ == "__main__":
