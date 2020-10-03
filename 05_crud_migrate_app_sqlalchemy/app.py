c245abdf4ca0ee484e20ffce97ccc018ede40c60
bc-maia <bruno_engineer@hotmail.com>
2020-10-03 15:09:00 -0300

    added - migrations support

--- 05_crud_migrate_app_sqlalchemy/app.py
+++ 05_crud_migrate_app_sqlalchemy/app.py
@@ -1,10 +1,12 @@
 import json
 import sys
 from flask import Flask, request, render_template, jsonify, abort
-
-# from flask import redirect, url_for
 from flask.wrappers import Response
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# TODO: uncomment the line bellow when using form:
+#       from flask import redirect, url_for
 
 
 app = Flask(__name__)
@@ -12,21 +14,23 @@ app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "postgresql://postgres:passwd123@localhost:5432/todoapp"
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    # completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return f"<To-Do ID: {self.id}, Description: {self.description}>"
 
 
-db.create_all()
+# db.create_all() TODO: used when there's not migration management
 
 
-# TODO: only used via form post
+# TODO: uncomment code-block bellow when using form:
 # @app.route("/todo/create", methods=["POST"])
 # def create():
 #     if request.method == "POST":
@@ -37,7 +41,8 @@ db.create_all()
 
 @app.route("/")
 def index():
-    return render_template("index.html", context=Todo.query.all())
+    todos = Todo.query.all()
+    return render_template("index.html", context=todos)
 
 
 @app.route("/api/todo", methods=["GET", "POST"])
