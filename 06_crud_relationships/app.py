04490e61f7f5590373f94ac579872511345cc89e
bc-maia <bruno_engineer@hotmail.com>
2020-10-04 17:58:51 -0300

    update - added todolists model

--- 06_crud_relationships/app.py
+++ 06_crud_relationships/app.py
@@ -4,9 +4,10 @@ from flask import Flask, request, render_template, jsonify, abort
 from flask.helpers import url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy.orm import backref
 from werkzeug.utils import redirect
 
-
+# Config
 app = Flask(__name__)
 app.config[
     "SQLALCHEMY_DATABASE_URI"
@@ -15,22 +16,29 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+# Models
+class TodoList(db.Model):
+    __tablename__ = "todolists"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship("Todo", backref="list", lazy=True)
+
+    def __repr__(self):
+        return f"<To-Do List ID: {self.id}, Name: {self.name}, To Do's: {self.todos}>"
+
+
 class Todo(db.Model):
     __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey("todolists.id"), nullable=False)
 
     def __repr__(self):
         return f"<To-Do ID: {self.id}, Description: {self.description}, Status: {self.completed}>"
 
 
-@app.route("/")
-def index():
-    todos = Todo.query.order_by("id").all()
-    return render_template("index.html", context=todos)
-
-
+# routes
 @app.route("/api/todo", methods=["GET"])
 def api_todo():
     todos = Todo.query.all()
@@ -93,6 +101,17 @@ def api_todo_update(todo_id):
             db.session.close()
 
         return jsonify({"success": success}) if success else abort(400)
+
+
+@app.route("/api/lists/<list_id>")
+def get_list_todos(list_id):
+    todos = Todo.query.filter_by(list_id=list_id).order_by("id").all()
+    return render_template("index.html", context=todos)
+
+
+@app.route("/")
+def index():
+    return redirect(url_for("get_list_todos", list_id=1))
 
 
 if __name__ == "__main__":
